fcef355f9c53766741e35e21759c1d53ff651bc1
Nelly J. Pacheco-Cruz <n311pc@gmail.com>
2025-05-03 23:50:11 -0600

    update sequences file without amb

--- bin/2.5.snps_outliers_sequences.R
+++ bin/2.5.snps_outliers_sequences.R
@@ -72,3 +72,52 @@ write_xlsx(unique_snps_with_sequences, "../results/consolidated_snps_with_sequen
 
 # Mensaje de confirmación
 cat("El archivo con las secuencias incorporadas se ha exportado a '../results/consolidated_snps_with_sequences.xlsx'.\n")
+
+# ---------------------------------------------------------------
+# Descripción: Contar caracteres ambiguos en cada secuencia y exportar el resultado.
+# ---------------------------------------------------------------
+
+# Contar caracteres ambiguos en cada secuencia
+unique_snps_with_sequences <- unique_snps_with_sequences %>%
+  mutate(
+    amb_R = str_count(sequence, "R"),  # Contar R
+    amb_Y = str_count(sequence, "Y"),  # Contar Y
+    amb_S = str_count(sequence, "S"),  # Contar S
+    amb_W = str_count(sequence, "W"),  # Contar W
+    amb_K = str_count(sequence, "K"),  # Contar K
+    amb_M = str_count(sequence, "M"),  # Contar M
+    total_ambiguities = amb_R + amb_Y + amb_S + amb_W + amb_K + amb_M  # Sumar todas las ambigüedades
+  )
+
+# Exportar el data frame actualizado con las secuencias y los conteos como archivo Excel
+write_xlsx(unique_snps_with_sequences, "../results/consolidated_snps_with_sequences_and_ambiguities.xlsx")
+
+# Mensaje de confirmación
+cat("El archivo con las secuencias, los conteos de caracteres ambiguos y el total de ambigüedades se ha exportado a '../results/consolidated_snps_with_sequences_and_ambiguities.xlsx'.\n")
+
+# ---------------------------------------------------------------
+# Script: Sustituir ambigüedades en las secuencias del archivo FASTA
+# Descripción: Este script procesa el archivo FASTA, sustituye los caracteres
+#              ambiguos por los nucleótidos especificados y guarda un nuevo archivo.
+# ---------------------------------------------------------------
+
+# Cargar el archivo de secuencias en formato FASTA
+fasta_lines <- readLines("../results/locus_names_for_sequences.txt")
+
+# Identificar líneas con secuencias (no encabezados)
+sequence_lines <- !grepl("^>", fasta_lines)
+
+# Sustituir las ambigüedades en las secuencias
+fasta_lines[sequence_lines] <- fasta_lines[sequence_lines] %>%
+  str_replace_all("R", "A") %>%
+  str_replace_all("Y", "C") %>%
+  str_replace_all("S", "G") %>%
+  str_replace_all("W", "T") %>%
+  str_replace_all("K", "G") %>%
+  str_replace_all("M", "A")
+
+# Guardar el archivo con las secuencias corregidas
+writeLines(fasta_lines, "../results/locus_names_for_sequences_wo_amb.txt")
+
+# Mensaje de confirmación
+cat("El archivo con las secuencias corregidas se ha exportado a '../results/locus_names_for_sequences_wo_amb.txt'.\n")
